6bbf62ac390ad3a42bac27c9b2a81a72915b2f57
Anton Koukharski <akokarski@gmail.com>
2020-08-13 15:09:22 -0700

    counter code

---
+++ flowcounter.py
@@ -0,0 +1,95 @@
+import sys, _thread, time, datetime, os, atexit
+import RPi.GPIO as GPIO
+from Adafruit_IO import Client
+
+import logging
+
+
+FLOW_SENSOR = 23
+
+
+class FlowCounter:
+
+
+    def __init__(self, aioClient, countFactor, flushMs, logger): 
+
+        self.logger = logger #logging.getLogger()
+        self.logger.info('countFactor: %f' % countFactor)
+        self.logger.info('flush miliseconds: %d' % flushMs)
+        self.flushMs = 500
+        if flushMs != None:
+            self.flushMs = flushMs
+
+        self.countFactor = countFactor
+
+        self.startTs = 0
+        self.lastTs = 0
+        self.count = 0
+
+        self.initGPIO()
+        self.aio = aioClient
+
+        def flusher():
+            while True:
+                time.sleep(self.flushMs / 100)
+                self.doFlush()
+            
+        _thread.start_new_thread(flusher, ())
+
+    def initGPIO(self):
+        self.logger.info('initializing gpio')
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=self.countPulse)
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        GPIO.cleanup()
+
+    def countPulse(self, arg):
+        now = self.nowMs()
+        if self.startTs == 0:
+            self.startTs = now
+        self.lastTs = now 
+        self.count += 1
+
+
+    def doFlush(self):
+
+        deltaMs = self.nowMs() - self.lastTs
+
+
+        if self.count > 0 and (deltaMs > self.flushMs):
+            flowTime = self.lastTs - self.startTs
+            flowStart = datetime.datetime.fromtimestamp(self.startTs/1000.0)
+
+            volume = self.count * self.countFactor
+
+            msg = '%s count: %d, vol: %f, timeMs: %d' % (flowStart, self.count, volume, flowTime)
+            self.logger.info(msg)
+            self.aio.send('water-meter-count', self.count)
+            self.aio.send('water-meter-volume', volume)
+            self.aio.send('water-meter-time', flowTime)
+            #self.logToFile(msg)
+            self.count = 0
+            self.startTs = 0
+            self.lastTs = 0
+
+    def logToFile(self, msg):
+        #filepath = '/tmp/flowd/log.txt'
+        filepath = '/var/lib/flowd/log.txt'
+        dirpath = os.path.dirname(filepath)
+
+        if not os.path.exists(dirpath) or not os.path.isdir(dirpath):
+            os.makedirs(dirpath)
+        f = open(filepath, 'a')
+        f.write(msg + "\n")
+        f.close()
+
+    def nowMs(self):
+        return int(round(time.time() * 1000))
+
+
+
+if __name__ == "__main__":
+    sys.exit(1)
---
+++ water-meter.py
@@ -0,0 +1,68 @@
+import logging
+from logging.handlers import RotatingFileHandler 
+import time
+import os
+import configparser
+from service import Service
+from Adafruit_IO import Client
+from flowcounter import FlowCounter
+
+
+class MyService(Service):
+    def __init__(self, *args, **kwargs):
+        super(MyService, self).__init__(*args, **kwargs)
+        rfh = RotatingFileHandler("/var/log/water-meter/water-meter.log", mode='a', maxBytes=10000, backupCount=3, 
+							encoding=None, delay=False)
+        
+        rfh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(rfh)
+        self.logger.setLevel(logging.INFO)
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+
+        self.logger.info("initialized...")
+
+
+    def run(self):
+        self.logger.info("run start");
+        self.aio = Client(self.config['adafruit.io']['user'], self.config['adafruit.io']['api_key']) 
+        self.fc = FlowCounter(self.aio, float (1) / 450, 200, self.logger)
+        while not self.got_sigterm():
+            self.log_temp()
+            time.sleep(5)
+        self.logger.info("run done");
+
+
+    def log_temp(self):
+
+        try:
+            tf = open('/sys/class/thermal/thermal_zone0/temp')
+            temp = float(tf.read())
+            tempC = temp/1000
+            self.logger.info("cpu temp %f" % tempC)
+            self.aio.send('water-meter', tempC)
+
+        except:
+            self.logger.err("Problem reading a thermal file")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        sys.exit('Syntax: %s COMMAND' % sys.argv[0])
+
+    cmd = sys.argv[1].lower()
+    service = MyService('water-meter', pid_dir='/tmp')
+
+    if cmd == 'start':
+        service.start()
+    elif cmd == 'stop':
+        service.stop()
+    elif cmd == 'status':
+        if service.is_running():
+            print ("Service is running with PID.", service.get_pid());
+        else:
+            print ("Service is not running.");
+    else:
+        sys.exit('Unknown command "%s".' % cmd)
